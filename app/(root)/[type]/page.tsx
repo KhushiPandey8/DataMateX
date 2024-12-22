c22976da47e5f33d9fab0d96757034a50e865880
xing24xing <khushipandey260304@gmail.com>
2024-12-22 21:36:42 +0530

    fix view files issues

--- app/(root)/[type]/page.tsx
+++ app/(root)/[type]/page.tsx
@@ -1,14 +1,17 @@
-import React from "react"
-import Sort from "@/components/Sort"
-import { getFiles } from "@/lib/actions/file.actions"
-import { Models } from "node-appwrite"
-import Card from "@/components/Card"
-import { getFileTypesParams } from "@/lib/utils"
+import React from "react";
+import Sort from "@/components/Sort";
+import { getFiles } from "@/lib/actions/file.actions";
+import { Models } from "node-appwrite";
+import Card from "@/components/Card";
+import { getFileTypesParams } from "@/lib/utils";
+
 const Page = async ({ searchParams, params }: SearchParamProps) => {
   const type = ((await params)?.type as string) || "";
   const searchText = ((await searchParams)?.query as string) || "";
   const sort = ((await searchParams)?.sort as string) || "";
+
   const types = getFileTypesParams(type) as FileType[];
+
   const files = await getFiles({ types, searchText, sort });
 
   return (
